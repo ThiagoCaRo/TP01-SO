ebebe5409a7a510dc37ad98fa39aae25f9503087
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2022-06-18 22:21:30 -0300

    Create definitions.h

---
+++ application/definitivo/definitions.h
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <inttypes.h>
+#include <signal.h>
+#include <stdbool.h>
+#define THREADSTACKSIZE    1024
+#define true    1
+#define false    0
+#define MAX_MEM    100
+#define MAX_LINHA    50  
+#define PRONTO    'P'
+#define EXECUCAO    'E'
+#define BLOQUEADO    'O'
+
+
+typedef struct{
+    pid_t pid;
+    pid_t ppid;
+    int pc;    // Initially started from zero.
+    int valor;
+    int prioridade;
+    char state;
+    int t_start;
+    int t_used;
+    char nome_arquivo[MAX_MEM]; 
+    char programa[MAX_MEM][MAX_LINHA];
+}Processos;
+
+typedef struct{
+    pid_t EXEC;
+    int pc;
+    int n;
+    pid_t pid;
+    char programa[MAX_MEM];
+    int X[MAX_MEM];
+    int indice, valor;
+}CPU;
+
+typedef struct fila{
+    Processos proc[MAX_MEM];
+}FILA;
+void transfere_tabela(CPU *cpu, Processos *proc);
+void transfere_cpu(Processos *proc, CPU *cpu);
+int ler_programa(char *file, char programa[MAX_MEM][MAX_LINHA]);
+Processos duplica_processo(Processos *proc, pid_t novo);
+Processos criar_processo(pid_t pid, pid_t ppid, int pc, int valor, char *nome_arquivo);
+void processo_main(int file_descriptor);
+void gerenciador_processos(int file_descriptor);
