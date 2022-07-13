001c061b17ad48db4a897c5be5415f7f79dcbb90
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2022-07-13 09:14:35 -0300

    Update MemoriaP.c

--- application/refactor/src/MemoriaPrincipal/MemoriaP.c
+++ application/refactor/src/MemoriaPrincipal/MemoriaP.c
@@ -1,7 +1,23 @@
 #include "MemoriaP.h"
 
-void preenche_pagina(int tamanho_pagina){
-  
-  
-  
+void inicializa_vazia(MEMORIA_PRINCIPAL *memoriaPrincipal){
+    memoriaPrincipal->pageframe[1].index = MAX_MEM;
+    memoriaPrincipal->pageframe[0].tamanho = MAX_MEM;
+    memoriaPrincipal->pageframe[0].index = 0;
+
+}
+
+int preenche_pagina(int tamanho_pagina){
+
+}
+
+void FirstFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina){
+    char op = 'A';
+    for(int i = 0; i<MAX_MEM; i++){
+        if(tamanho_pagina < memoriaPrincipal->pageframe[i].tamanho){
+
+        }
+        
+    }
+
 }
