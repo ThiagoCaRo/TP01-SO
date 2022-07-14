634326cdb2c67c0d2479b47382ce88f8782f6d74
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2022-07-14 10:20:37 -0300

    Update MemoriaP.c

--- application/refactor/src/MemoriaPrincipal/MemoriaP.c
+++ application/refactor/src/MemoriaPrincipal/MemoriaP.c
@@ -4,18 +4,34 @@ void inicializa_vazia(MEMORIA_PRINCIPAL *memoriaPrincipal){
     memoriaPrincipal->pageframe[1].index = MAX_MEM;
     memoriaPrincipal->pageframe[0].tamanho = MAX_MEM;
     memoriaPrincipal->pageframe[0].index = 0;
+    memoriaPrincipal->pageframe[0].ocupado = false;
 
 }
 
-int preenche_pagina(int tamanho_pagina){
+int preenche_pagina(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, int index_pageframe, char operador){
+    int tamanho_atual, index_atual;
 
+    memoriaPrincipal->pageframe[index_pageframe].tamanho = tamanho_pagina;
+
+    tamanho_atual = memoriaPrincipal->pageframe[index_pageframe].tamanho;
+
+    memoriaPrincipal->pageframe[index_pageframe+1].index = memoriaPrincipal->pageframe[index_pageframe].tamanho + memoriaPrincipal->pageframe[index_pageframe].index;
+
+    index_atual = memoriaPrincipal->pageframe[index_pageframe].index;
+    switch (operador) { //Possivelmente o Switch nao sera necessario
+        case 'A':
+            for(int i = index_atual; i < tamanho_atual; i++){
+
+            }
+    }
 }
 
 void FirstFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina){
     char op = 'A';
     for(int i = 0; i<MAX_MEM; i++){
-        if(tamanho_pagina < memoriaPrincipal->pageframe[i].tamanho){
-
+        if(tamanho_pagina < memoriaPrincipal->pageframe[i].tamanho && memoriaPrincipal->pageframe[i].ocupado == false){
+            preenche_pagina(memoriaPrincipal, tamanho_pagina, i, op);
+            break;
         }
         
     }
