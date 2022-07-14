a736e0a9d7c5fe02a7903eda15782df74a6e07e0
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2022-07-14 10:20:16 -0300

    Update MemoriaP.h

--- application/refactor/src/MemoriaPrincipal/MemoriaP.h
+++ application/refactor/src/MemoriaPrincipal/MemoriaP.h
@@ -2,18 +2,23 @@
 #define __MEMORIAP_H__
 
 #include "../Fila/Fila.h"
+#define true    1
+#define false    0
 
 typedef struct{
     int tamanho;
     int index;
+    bool ocupado;
 }frames;
 
 typedef struct{
     int RAM[MAX_MEM];
-    frames page_frame[MAX_MEM];
+    frames pageframe[MAX_MEM];
 
 }MEMORIA_PRINCIPAL;
 
 void inicializa_vazia(MEMORIA_PRINCIPAL *memoriaPrincipal);
-int preenche_pagina(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, char operador);
+int preenche_pagina(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, int index_pageframe, char operador);
 void FirstFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina);
+
+#endif // __MEMORIAP_H__
