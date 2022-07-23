e6a42fe40df741491e3fb4603472c6cb72d45bf2
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2022-07-23 09:44:00 -0300

    Update Arquivo.h

--- application/refactor/src/Arquivo/Arquivo.h
+++ application/refactor/src/Arquivo/Arquivo.h
@@ -2,6 +2,6 @@
 #define __ARQUIVO_H__
 #include "../Constantes/Constantes.h"
 
-int ler_programa(char *file, char programa[MAX_MEM][MAX_LINHA]);
+int ler_programa(char *file, char programa[MAX_MEM][MAX_LINHA]);//recebe ponteiro para arquivo e a matriz que armazenará o programa inteiro
 
 #endif // __ARQUIVO_H__
