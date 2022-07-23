591da8831a05e539df523e23879ac537eb290f36
EduardoTTristao <89053092+EduardoTTristao@users.noreply.github.com>
2022-07-23 10:23:12 -0300

    colocando comentarios

--- application/refactor/src/MemoriaPrincipal/MemoriaP.c
+++ application/refactor/src/MemoriaPrincipal/MemoriaP.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 void inicializa_vazia(MEMORIA_PRINCIPAL *memoriaPrincipal){
-    for(int i=1; i<MAX_MEM; i++){
+    for(int i=1; i<MAX_MEM; i++){ // coloca o indice de todos os vaos como vazios
         memoriaPrincipal->pageframe[i].index = -1;
     }
-    memoriaPrincipal->pageframe[0].tamanho = MAX_RAM;
+    memoriaPrincipal->pageframe[0].tamanho = MAX_RAM; // coloca o primeiro vao como sendo a memoria inteira
     memoriaPrincipal->pageframe[0].index = 0;
 
 }
@@ -14,10 +14,10 @@ void inicializa_vazia(MEMORIA_PRINCIPAL *memoriaPrincipal){
 int preenche_pagina(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, int index_pageframe){
     int index_inicial;
 
-    memoriaPrincipal->pageframe[index_pageframe].tamanho -= tamanho_pagina;
+    memoriaPrincipal->pageframe[index_pageframe].tamanho -= tamanho_pagina; // reduz o tamanho do vao de acordo com o que vc quer alocar
     index_inicial = memoriaPrincipal->pageframe[index_pageframe].index;
     
-    if (memoriaPrincipal->pageframe[index_pageframe].tamanho == 0){
+    if (memoriaPrincipal->pageframe[index_pageframe].tamanho == 0){ // caso o vao tenha tamanho igual a 0 ele é excluido
         for(int i = 0; i < MAX_MEM-1; i++){
            if(memoriaPrincipal->pageframe[i].index >= memoriaPrincipal->pageframe[index_pageframe].index){
      
@@ -27,9 +27,9 @@ int preenche_pagina(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, int
         }
     }
     else
-    	memoriaPrincipal->pageframe[index_pageframe].index += tamanho_pagina;
+    	memoriaPrincipal->pageframe[index_pageframe].index += tamanho_pagina; // caso nao, ele apenas tem seu index inicial mudado
 
-    return index_inicial;
+    return index_inicial; // retorna o antigo index inicial
 }
 
 int Fit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, char tipo, int *next){
@@ -49,7 +49,7 @@ int Fit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, char tipo, int 
 int NextFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, int *next){
     int i = *next;
     while(1){
-        if(tamanho_pagina <= memoriaPrincipal->pageframe[i].tamanho){
+        if(tamanho_pagina <= memoriaPrincipal->pageframe[i].tamanho){ // procura o proximo que esta vazio e preenche or espectivo vao
             *next = i;
             return preenche_pagina(memoriaPrincipal, tamanho_pagina, i);
         }
@@ -66,7 +66,7 @@ int BestFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina){
     for(int i = 0; i<MAX_MEM; i++){
         if(((min == -1) | (memoriaPrincipal->pageframe[min].tamanho > memoriaPrincipal->pageframe[i].tamanho))
             && tamanho_pagina <= memoriaPrincipal->pageframe[i].tamanho
-            && memoriaPrincipal->pageframe[i].index != -1){
+            && memoriaPrincipal->pageframe[i].index != -1){ //  procura o menor que caiba o que vc quer alocar e preenche o vao
             min = i;
         }
     }
@@ -80,7 +80,7 @@ int WorstFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina){
     for(int i = 0; i<MAX_MEM; i++){
         if(((max == -1) | (memoriaPrincipal->pageframe[max].tamanho < memoriaPrincipal->pageframe[i].tamanho))
             && tamanho_pagina <= memoriaPrincipal->pageframe[i].tamanho
-            && memoriaPrincipal->pageframe[i].index != -1){
+            && memoriaPrincipal->pageframe[i].index != -1){ //procura o maior que caiba o que voce quer alocar e preenche o vao
             max = i;
         }
     }
@@ -91,7 +91,7 @@ int WorstFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina){
 
 int FirstFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina){
     for(int i = 0; i<MAX_MEM; i++){
-        if(tamanho_pagina <= memoriaPrincipal->pageframe[i].tamanho){
+        if(tamanho_pagina <= memoriaPrincipal->pageframe[i].tamanho){ // procura o primeiro que caiba e preenche o vao
             return preenche_pagina(memoriaPrincipal, tamanho_pagina, i);
         }
         
@@ -101,46 +101,46 @@ int FirstFit(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina){
 }
 
 int removePagina(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, int index){
-    for(int i=0;i<MAX_MEM-1;i++){
+    for(int i=0;i<MAX_MEM-1;i++){ // procura a vizinhanca do vao que voce quer remover
         if(memoriaPrincipal->pageframe[i].index + memoriaPrincipal->pageframe[i].tamanho == index && 
-            memoriaPrincipal->pageframe[i+1].index == index + tamanho_pagina){
+            memoriaPrincipal->pageframe[i+1].index == index + tamanho_pagina){ // caso exista uma atras e um na frente
             
-                memoriaPrincipal->pageframe[i].tamanho += tamanho_pagina + memoriaPrincipal->pageframe[i+1].tamanho;
-                for(int j = i+1; j < MAX_MEM-1; j++){
+                memoriaPrincipal->pageframe[i].tamanho += tamanho_pagina + memoriaPrincipal->pageframe[i+1].tamanho; // o mais atras abrange os dois novos da frente
+                for(int j = i+1; j < MAX_MEM-1; j++){ // retira o mais a frente com um shift no vetor
                     memoriaPrincipal->pageframe[j] = memoriaPrincipal->pageframe[j+1];
                 }
                 break;
         }
-        if(memoriaPrincipal->pageframe[i].index + memoriaPrincipal->pageframe[i].tamanho == index){
-            memoriaPrincipal->pageframe[i].tamanho += tamanho_pagina;
+        if(memoriaPrincipal->pageframe[i].index + memoriaPrincipal->pageframe[i].tamanho == index){ // caso exista somente atras
+            memoriaPrincipal->pageframe[i].tamanho += tamanho_pagina; // o de tras abrange o espaco desalocado
             break;
         }
-        if(memoriaPrincipal->pageframe[i+1].index == index + tamanho_pagina){
-            memoriaPrincipal->pageframe[i+1].index-=tamanho_pagina;
-            memoriaPrincipal->pageframe[i+1].tamanho+=tamanho_pagina;
+        if(memoriaPrincipal->pageframe[i+1].index == index + tamanho_pagina){ // caso exista somente na frente
+            memoriaPrincipal->pageframe[i+1].index-=tamanho_pagina; // o ponteiro inicial do vao recua com o espaco desalocado
+            memoriaPrincipal->pageframe[i+1].tamanho+=tamanho_pagina; // o tamanho do vao aumenta proporcionalmente
             break;
         }
-        if (memoriaPrincipal->pageframe[i].index == index + tamanho_pagina && i == 0){
+        if (memoriaPrincipal->pageframe[i].index == index + tamanho_pagina && i == 0){ // caso so exista na frente, mas na frente é o primeiro no vetor
             memoriaPrincipal->pageframe[i].index-=tamanho_pagina;
             memoriaPrincipal->pageframe[i].tamanho+=tamanho_pagina;
             break;
         }
         if ((memoriaPrincipal->pageframe[i].index + memoriaPrincipal->pageframe[i].tamanho < index &&
                 (memoriaPrincipal->pageframe[i+1].index > index + tamanho_pagina || memoriaPrincipal->pageframe[i+1].index == -1)) ||
-                (memoriaPrincipal->pageframe[i].index > index && memoriaPrincipal->pageframe[i+1].index == -1)){
+                (memoriaPrincipal->pageframe[i].index > index && memoriaPrincipal->pageframe[i+1].index == -1)){ // caso nao exista atras e nem na frente
 
-            frames pagina;
+            frames pagina; // cria a pagina com o tamanho do espaco desalocado e seu respectivo endereco
             pagina.index = index;
             pagina.tamanho = tamanho_pagina;
-            bool flag = true;
-            for(int j = MAX_MEM-2; j >= 0; j--){
+            bool flag = true; // flag para caso exista nenhuma pagina
+            for(int j = MAX_MEM-2; j >= 0; j--){ // shift para a direita no vetor
                 if(memoriaPrincipal->pageframe[j].index > pagina.index){
 
                     memoriaPrincipal->pageframe[j+1] = memoriaPrincipal->pageframe[j];
 
                 }
                 else{
-                    if(flag && memoriaPrincipal->pageframe[j].index != -1){
+                    if(flag && memoriaPrincipal->pageframe[j].index != -1){ // coloca o novo vao no espaco novo
                         flag = false;
                         memoriaPrincipal->pageframe[j+1] = pagina;
                         break;
@@ -157,7 +157,7 @@ int removePagina(MEMORIA_PRINCIPAL *memoriaPrincipal, int tamanho_pagina, int in
 
     }
     for(int i = index; i < index + tamanho_pagina; i++){
-        memoriaPrincipal->RAM[i] = 0;
+        memoriaPrincipal->RAM[i] = 0; // esvazia os valores da ram
     }
     
     return 0;
