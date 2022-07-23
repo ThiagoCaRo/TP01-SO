96123ed27ef37ea349b4249ca0bd125ed9efb167
EduardoTTristao <89053092+EduardoTTristao@users.noreply.github.com>
2022-07-23 10:41:40 -0300

    adicionando comentarios

--- application/refactor/src/Constantes/Constantes.h
+++ application/refactor/src/Constantes/Constantes.h
@@ -1,5 +1,5 @@
-#define MAX_RAM    2
+#define MAX_RAM    2 // tamanho da ram
 #define MAX_LINHA    50
 #define MAX_MEM    100
-#define MAX_VIR 50
-#define MAX_DISC 100
+#define MAX_VIR 50 // tamanho da memoria virtual
+#define MAX_DISC 100 // tamanho do disco
